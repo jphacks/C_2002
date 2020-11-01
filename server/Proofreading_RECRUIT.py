e4a3ab1cb16de45de0331e9401c583972d23fef6
RyogaTakao <170441089@ccalumni.meijo-u.ac.jp>
2020-11-02 02:29:13 +0900

    固有名詞抽出と文章校正のRestAPI実装完了！

--- server/Proofreading_RECRUIT.py
+++ server/Proofreading_RECRUIT.py
@@ -3,6 +3,7 @@
 import os
 import configparser
 import requests
+import json
 
 # ソースファイルの場所取得
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -19,18 +20,38 @@ headers= {
 }
 
 # 校正支援をJSON形式で取得する関数
-def run_roofreading(sentence):
-    # Proofreading APIへのGET通信
-    response_proofreading = requests.get(RECRUITE_API_PROOFREADING_URL + '?apikey=' + RECRUITE_API_PROOFREADING_API_KEY + '&sentence=' + sentence)
-    return response_proofreading.text
+def get_json_roofreading(text):
+    sentences = text.split('。')
+    response_list = []
+    result_list = []
+    for sentence in sentences:
+        # Proofreading APIへのGET通信
+        response_proofreading = requests.get(RECRUITE_API_PROOFREADING_URL + '?apikey=' + RECRUITE_API_PROOFREADING_API_KEY + '&sentence=' + sentence)
+        response_list.append(response_proofreading.json())
+    # print(response_list)
+    for res in response_list:
+        if res['message']!='ok':
+            result = {
+                'inputSentence': res['inputSentence'],
+                'checkedSentence': res['checkedSentence'],
+                'alerts': res['alerts']
+            }
+            result_list.append(result)
+    # return json.dumps(result_list, ensure_ascii=False)
+    return result_list
 
 sentence = "システムの企画から開発・運用まで幅広く関われまs。"
 # sentence = "システムの規格から開発・運用まで幅広く関われます。"
+# response_proofreading = requests.get(RECRUITE_API_PROOFREADING_URL + '?apikey=' + RECRUITE_API_PROOFREADING_API_KEY + '&sentence=' + sentence)
+# print(response_proofreading.json())
+# テキストファイルの読み込み
+sentence = open(APP_ROOT + "/sample.txt", "r", encoding="utf-8").read().replace('\n','')
 
 # 校正支援をJSON形式で取得
-result_calibration_json = run_roofreading(sentence)
+result_calibration_json = get_json_roofreading(sentence)
 result_calibration_json = {
     'calibration': result_calibration_json
 }
-res = requests.post('http://localhost:5000/test', result_calibration_json, headers)
-print(res)
+print(result_calibration_json)
+# res = requests.post('http://localhost:5000/test', result_calibration_json, headers)
+# print(res)
--- server/server.py
+++ server/server.py
@@ -1,11 +1,12 @@
 # coding: UTF-8
 
-from flask import Flask, request, jsonify
+from flask import Flask, request
 import os
 import configparser
 from goolabs import GoolabsAPI
 import json
 import requests
+import re
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
@@ -34,14 +35,34 @@ def get_json_people_companies(sentence):
     response = gooAPI.entity(sentence=sentence)
     people_name = list(set([people[0] for people in response['ne_list'] if people[1]=='PSN']))
     companies_name = list(set([company[0] for company in response['ne_list'] if company[1]=='ORG']))
-    return json.dumps(people_name, ensure_ascii=False), json.dumps(companies_name, ensure_ascii=False)
+    return people_name, companies_name
 
+# 校正支援をJSON形式で取得する関数
+def get_json_roofreading(text):
+    sentences = re.split('[、。，．,.!！？?"「」]', text)
+    response_list = []
+    result_list = []
+    for sentence in sentences:
+        # Proofreading APIへのGET通信
+        response_proofreading = requests.get(RECRUITE_API_PROOFREADING_URL + '?apikey=' + RECRUITE_API_PROOFREADING_API_KEY + '&sentence=' + sentence)
+        response_list.append(response_proofreading.json())
+    for res in response_list:
+        if res['message']!='ok':
+            result = {
+                'inputSentence': res['inputSentence'],
+                'checkedSentence': res['checkedSentence'],
+                'alerts': res['alerts']
+            }
+            result_list.append(result)
+    return result_list
 
+# ローカルGETテスト用
 @app.route('/')
 def hello():
     name = "Hello World"
     return name
 
+# ローカルPOSTテスト用
 @app.route('/test', methods=['POST'])
 def post_test():
     # Node.jsからのPOST
@@ -54,17 +75,40 @@ def post_test():
     # print(request.form['companies_name'])
     return 'Success'
 
-@app.route('/getdata', methods=['POST'])
+@app.route('/getdata')
 def post_getdata():
-    # Node.jsからのPOST
-    json_file = request.get_json()
-    print(json_file['people_name'])
-    print(json_file['companies_name'])
+    # テキストファイルの読み込み
+    sentence = open(APP_ROOT + "/sample.txt", "r", encoding="utf-8").read().replace('\n','')
 
-    # PythonからのPOST
-    # print(request.form['people_name'])
-    # print(request.form['companies_name'])
-    return 'Success'
+    # 人名と会社名をJSON形式で取得
+    people_name_json, companies_name_json = get_json_people_companies(sentence)
+    # 校正支援をJSON形式で取得
+    result_calibration_json = get_json_roofreading(sentence)
+
+    # 全て結果をJSON形式にまとめて返す
+    result = {
+        'people_name': people_name_json,
+        'companies_name': companies_name_json,
+        'calibration': result_calibration_json
+    }
+    return result
+
+@app.route('/postdata', methods=['POST'])
+def post_postdata():
+    json_post = request.get_json()
+    sentence = json_post['sentence'].replace('\n','')
+
+    # 人名と会社名をJSON形式で取得
+    people_name_json, companies_name_json = get_json_people_companies(sentence)
+    # 校正支援をJSON形式で取得
+    result_calibration_json = get_json_roofreading(sentence)
+    # 全て結果をJSON形式にまとめて返す
+    result = {
+        'people_name': people_name_json,
+        'companies_name': companies_name_json,
+        'calibration': result_calibration_json
+    }
+    return result
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, threaded=True)
