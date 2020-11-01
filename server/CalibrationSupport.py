4eed093b795f7225dc724b13568d39112c599abc
RyogaTakao <170441089@ccalumni.meijo-u.ac.jp>
2020-11-01 17:17:46 +0900

    校正支援動作確認完了！ でも精度がビミョーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー

--- server/CalibrationSupport.py
+++ server/CalibrationSupport.py
@@ -3,8 +3,11 @@
 import os
 import configparser
 import requests
-import urllib.parse
-
+import xmljson
+from lxml.etree import parse
+import json
+import xmltodict
+import xml.etree.ElementTree as ET
 
 # ソースファイルの場所取得
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -15,11 +18,28 @@ config.read(APP_ROOT + '/config_local.ini')
 YAHOO_API_CLIENT_ID = config.get("Yahoo_API", "ClientId")
 YAHOO_API_CALIBRATION_SUPPORT_URL = config.get("Yahoo_API", "CalibrationSupportURL")
 
-sentence = "私はその人の記憶を呼び起すごとに、すぐ「先生」といいたくなる".replace('、', '')
+headers= {
+    "Content-type": "application/json"
+}
 
-# sentence = '遙か彼方に小形飛行機が見える'
+# 校正支援テスト用の文
+sentence = '遙か彼方に小形飛行機が見えた'
 
-response = requests.get(str(YAHOO_API_CALIBRATION_SUPPORT_URL) + '?appid=' + YAHOO_API_CLIENT_ID + '&sentence=' + sentence)
-
-# print(urllib.parse.quote(sentence))
-print(response.text)
+# YahooAPI校正支援へのGET通信
+response_calibration = requests.get(str(YAHOO_API_CALIBRATION_SUPPORT_URL) + '?appid=' + YAHOO_API_CLIENT_ID + '&sentence=' + sentence)
+# responseをXML形式へ変換
+response_calibration_xml = ET.fromstring(response_calibration.text)
+# XML形式をJSON形式へ変換
+list_calibration = []
+for results_calibration in response_calibration_xml:
+    result_calibration={}
+    for index_calibration in results_calibration:
+        # print(index_calibration.tag, index_calibration.text)
+        result_calibration[index_calibration.tag.split('}')[1]] = index_calibration.text
+    list_calibration.append(result_calibration)
+print(json.dumps(list_calibration))
+result_calibration_json = {
+    'calibration': json.dumps(list_calibration, ensure_ascii=False)
+}
+res = requests.post('http://localhost:5000/test', result_calibration_json, headers)
+print(res)
