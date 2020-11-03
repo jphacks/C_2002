1c40b982b780cdf19e30b4da97518e7374ac7c93
RyogaTakao <170441089@ccalumni.meijo-u.ac.jp>
2020-11-03 14:12:04 +0900

    HonorificsConvert.pyの修正

--- server/server.py
+++ server/server.py
@@ -56,6 +56,44 @@ def get_json_roofreading(text):
             result_list.append(result)
     return result_list
 
+# 単語探索関数
+def SearchForWords(sentence):
+       for start in range(len(sentence)):
+              for end in range(len(sentence) - 1, start - 1, -1):
+                     testKey = ''
+                     for check in range(start, end + 1):
+                            testKey += sentence[check][0]
+                     if testKey in HumbleLangDict:
+                            if testKey not in HitWordList:
+                                   HitWordList.append(testKey)
+
+# 単語置き換え用関数
+def ChangeWord(text, HitWordList):
+       ConvertedText = text
+       for word in HitWordList:
+              ConvertedText = ConvertedText.replace(word, HumbleLangDict[word])
+       return ConvertedText
+
+# 敬語変換関数
+def ChangeToHonorific(text):
+       # 辞書データ取得
+       json_open = open(APP_ROOT + '/sample.json', 'r')
+       global HumbleLangDict
+       HumbleLangDict = json.load(json_open)
+       print(json.dumps(HumbleLangDict, indent=2).encode().decode('unicode-escape'))
+       
+       global HitWordList
+       HitWordList = []
+
+       # See sample response below.
+       response = gooAPI.morph(sentence = text)
+       # 文章ごとに変換
+       for sentence in response['word_list']:
+             SearchForWords(sentence)
+       print(HitWordList)
+       ConvertedText = ChangeWord(text, HitWordList)
+       return ConvertedText
+
 # ローカルGETテスト用
 @app.route('/')
 def hello():
