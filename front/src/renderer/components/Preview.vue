9d822069766e23937a84d2b23f5b864c931711c4
ree-rishun <ree0432@gmail.com>
2020-11-05 02:44:49 +0900

    更新時間の表示

--- front/src/renderer/components/Preview.vue
+++ front/src/renderer/components/Preview.vue
@@ -2,7 +2,7 @@
   <div id="preview">
 
     <div class="upbar_list">
-      UPDATED
+      UPDATED {{ updated }}
     </div>
 
     <div class="upbar_list">
@@ -25,10 +25,36 @@
     },
     data () {
       return {
-        content: {
-          subject: 'テスト件名'
-        }
+        updated: ''
       }
+    },
+    methods: {
+      dateFormat (date, format = 'YYYY-MM-DD hh:mm:ss') {
+        // パース
+        format = format.replace(/YYYY/g, date.getFullYear())
+        format = format.replace(/MM/g, ('0' + (date.getMonth() + 1)).slice(-2))
+        format = format.replace(/DD/g, ('0' + date.getDate()).slice(-2))
+        format = format.replace(/hh/g, ('0' + date.getHours()).slice(-2))
+        format = format.replace(/mm/g, ('0' + date.getMinutes()).slice(-2))
+        format = format.replace(/ss/g, ('0' + date.getSeconds()).slice(-2))
+
+        if (format.match(/S/g)) {
+          let milliSeconds = ('00' + date.getMilliseconds()).slice(-3)
+          let length = format.match(/S/g).length
+          for (let i = 0; i < length; i++) format = format.replace(/S/, milliSeconds.substring(i, i + 1))
+        }
+
+        // 文字列を返す
+        return format
+      }
+    },
+    watch: {
+      'mailBody': function () {
+        this.updated = this.dateFormat(new Date())
+      }
+    },
+    mounted () {
+      this.updated = this.dateFormat(new Date())
     }
   }
 </script>
