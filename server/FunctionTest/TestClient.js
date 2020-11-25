0eb5dd88c517d7ffd609401219836e468a3d5102
RyogaTakao <170441089@ccalumni.meijo-u.ac.jp>
2020-11-26 03:30:51 +0900

    被りを削除

--- server/FunctionTest/TestClient.js
+++ server/FunctionTest/TestClient.js
@@ -46,23 +46,41 @@ function readUserInput(question) {
     //     });
     // }
 
-    // var sentence = '株式会社●●●●\n●●●●様\n\nお世話様です。\n株式会社Pied Piperの岡崎竜也です。\n\n先日は打ち合わせの時間をくれまして、誠にありがとうございます。\nご依頼された商品Aの見積りが用意できました。\n打ち合わせで聞いた要望を反映し、特別割引を設けております。\n詳細について説明させてほしいのですが、1時間ほど時間をください。\n来週でしたら以下の日程で行くことが可能です。\n＜候補日時＞　\n・●月●日（火）10:00～11:30\n・●月●日（水）14:00～17:00\n・●月●日（木）15:30～17:30\n・●月●日（金）10:00～15:00\n\n●●様の都合を聞かせてください。\n\n忙しいところすいませんが、ご検討よろしくお願いいたします。'
-    // var sentence = message.split('\n')
-    var sentence = '岡崎\n高尾\n名城大学\n近藤\n石倉\n南山大学\n大羽'
-
+    // 名称リスト取得テスト用
+    // var sentence = '株式会社●●●●\n●●●●様\n\nお世話様です。\n株式会社Pied Piperの岡崎です。岡崎\n高尾\n名城大学\n近藤\n石倉\n南山大学\n大羽\n\n先日は打ち合わせの時間をくれまして、誠にありがとうございます。\nご依頼された商品Aの見積りが用意できました。\n打ち合わせで聞いた要望を反映し、特別割引を設けております。\n詳細について説明させてほしいのですが、1時間ほど時間をください。\n来週でしたら以下の日程で行くことが可能です。\n＜候補日時＞　\n・●月●日（火）10:00～11:30\n・●月●日（水）14:00～17:00\n・●月●日（木）15:30～17:30\n・●月●日（金）10:00～15:00\n\n●●様の都合を聞かせてください。\n\n忙しいところすいませんが、ご検討よろしくお願いいたします。'
+    var sentence = '岡崎\n高尾\n名城大学\n大羽\n南山大学\n近藤\n石倉\n名城大学\n大羽'
     var options = {
-      uri: "http://0.0.0.0:5000/postnames",
       // uri: "http://54.64.167.36:5000/postnames",
-      // uri: "http://54.64.167.36:5000/postdata",
+      uri: "http://0.0.0.0:5000/postnames",
       headers: {
-          "Content-type": "application/json",
+        "Content-type": "application/json",
       },
       json: {
-          'sentence': sentence
+        'sentence': sentence
       }
     };
     console.log('Send { \nsentence: ' + sentence + '\n}')
     request.post(options, function(error, response, body){
-        console.log(body)
+      console.log(body)
     });
+
+    // 敬語変換テスト用
+    // var commit_id = '1c40b98';
+    // var sentence = '忙しいところすいませんが、ご検討よろしくお願いいたします。'
+
+    // var options = {
+    //   uri: "http://54.64.167.36:5000/postchange",
+    //  // uri: "http://0.0.0.0:5000/postchange
+    //   headers: {
+    //     "Content-type": "application/json",
+    //   },
+    //   json: {
+    //     'commit_id': commit_id,
+    //     'sentence': sentence
+    //   }
+    // };
+    // console.log('Send { \ncommit_id: ' + commit_id + '\nsentence: ' + sentence + '\n}')
+    // request.post(options, function(error, response, body){
+    //     console.log(body)
+    // });
 })();
